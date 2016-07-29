a7c801b0483027271afa4156a40727d1da5e360f
Programmer <programmer@programmers-mbp.nv.charter.com>
2016-07-28 19:24:44 -0700

    Test Change

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // change
+        // change 2
         // Do any additional setup after loading the view, typically from a nib.
     }
 
